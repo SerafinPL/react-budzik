c621b361558ea9cd897e425802f2a15af40eae17
serafinpl <ser.burberibar@gmail.com>
2021-01-17 20:45:41 +0100

    dokonczenie odliczania

--- src/minutnik/minutnik.js
+++ src/minutnik/minutnik.js
@@ -53,7 +53,7 @@ class Minutnik extends React.Component {
 			czasy : tablicaCzasow
 		});
 
-		console.log(this.state.czasy);		
+		
 
 	}
 
@@ -69,21 +69,84 @@ class Minutnik extends React.Component {
     	});
   	}
 
+
+  	czasowanie = () => {
+
+  	}
+
   	odliczanie = () => {
-  		const czasy = document.getElementsByClassName("wpisy");
+  		const czasy = this.state.czasy;
   		//const aktualnyCzas = document.getElementById("zegar").textContent;
-  		
+  		let noweCzasy = [];
+  		let skasowano; // zmienna zaznacza czy kasujemy ten element
   			for(let i = 0;i < czasy.length ; i++){
-  				let tg = czasy[i].textContent.slice(0,2);
+  				// v Wyodrębniamy godziny minuty i sekundy v
+  				let tg = Number(czasy[i].budzik.slice(0,2));
 
-  				let tm = czasy[i].textContent.slice(3,5);
+  				let tm = Number(czasy[i].budzik.slice(3,5));
 
-  				let ts = czasy[i].textContent.slice(6,8);
-  				console.log(tg,tm,ts);
+  				let ts = Number(czasy[i].budzik.slice(6,8));
   				
-  				//dopisac dalej
+  				
+  				let tns, tnm, tng;
+  				// v Robimy odliczanie
+  				tns = ts -1;
+  				if (ts > 0){
+  					tns = ts-1
+  					tnm = tm;
+  					tng = tg;
+  				} else if (tns == 0 && tm > 0 ){
+  					tns = 59;
+  					tnm = tm -1;
+  					tng = tg;
+  				} else if (tns == 0 && tm == 0 && tg > 0){
+  					tns = 59;
+  					tnm = 59;
+  					tng = tg - 1;
+  				} else if ( tns < 0 && tm == 0 && tg == 0){
+  					// jeżeli doszlo do zera to muzyka  i zaznaczamy aby sie 
+  					// nie wpisał na nowo
+					document.getElementById("audio").play();
+  					
+  					skasowano = i;
 
+  				}
 
+  				// ustawiamy pomniejszony stan
+  				let czas = "";
+
+				if (tng < 10){
+					czas = "0"; 
+				} 
+				czas += tng + ":";
+
+				if (tnm < 10){
+					czas += "0"; 
+				} 
+				czas += tnm + ":";
+
+				if (tns < 10){
+					czas += "0"; 
+				} 
+				czas += tns;
+				
+				if (i !== skasowano){ // sprawdzamy czy wpis nie jest skaowany żeby nie wszedl 
+									  // na nowo
+					if (i== 0){ // jeżeli pierwszy
+						noweCzasy[i] = {
+							budzik : czas,
+							klucz : czasy[i].klucz
+						}
+					}else{ // jeżeli kolejny to dodajemy
+						noweCzasy[i] += {
+							budzik : czas,
+							klucz : czasy[i].klucz
+						}
+					}
+				}
+				console.log(noweCzasy);
+  				console.log("g:" + tng + " m:" + tnm + " s:" + tns);
+  				
 
   				/*if (t == aktualnyCzas){
   					
@@ -91,6 +154,15 @@ class Minutnik extends React.Component {
   					window.alert(aktualnyCzas);
   				}*/
   			}
+  			//console.log(this.state.czasy);
+  			//console.log(noweCzasy);
+  		if (noweCzasy !== undefined){
+  			this.setState({
+      			czasy: noweCzasy
+    		});
+
+    	}
+  		
   		
   		
   	}
