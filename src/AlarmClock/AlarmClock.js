1521f8cfe78890f4fee7ca81edeb37732ff26a50
serafinpl <ser.burberibar@gmail.com>
2021-04-13 19:56:10 +0200

    changes to map function

--- src/AlarmClock/AlarmClock.js
+++ src/AlarmClock/AlarmClock.js
@@ -6,18 +6,7 @@ import Button from "../Button/Button.js";
 import Times from "../Times/Times.js";
 import classes from './AlarmClock.module.css';
 
-const AlarmClock = (props) => {//extends React.Component {
-	// constructor(props){
-	// 	super(props);
-
-	// 	//this.setter = this.setter.bind(this);
-	// 	//this.kasowanie = this.kasowanie.bind(this);
-		
-
-	// 	this.state = {
-	// 		times : []
-	// 	}
-	// }
+const AlarmClock = (props) => {
 
 	const [times, setTimes] = useState([]);
 
@@ -55,11 +44,7 @@ const AlarmClock = (props) => {//extends React.Component {
 
 		setTimes(tablicaCzasow);
 
-		// this.setState({
-		// 	times : tablicaCzasow
-		// });
-
-		//console.log(this.state.times);		
+				
 
 	} //setter
 
@@ -70,9 +55,7 @@ const AlarmClock = (props) => {//extends React.Component {
       		return (item.key !== keyring)
     	});
 
-    	// this.setState({
-     //  		times: filter
-    	// });
+    	
     	setTimes(filter);
 
   	} // deleting
@@ -81,18 +64,21 @@ const AlarmClock = (props) => {//extends React.Component {
   		const newTimes = [...times]//this.state.times //document.getElementsByClassName("wpisy");
   		const currentTime = document.getElementById("clock").textContent;
   		
-  		for(let i = 0;i < newTimes.length ; i++){
-  			
-  			let t = newTimes[i].alarmClock.slice(0,8);
+  		
+  		newTimes.map(index => {
+
+  			let t = index.alarmClock.slice(0,8);
 
   			if (t === currentTime){
-  					
   				document.getElementById("audio").play();
   				window.alert(currentTime);
-  				//this.deleting(newTimes[i].key);
-  				
+  				//deleting(newTimes[i].key);
   			}
-  		}
+  			return null;
+  		})
+  			
+  			
+  		
   	} // comparison
 
   	useEffect(() => {
@@ -100,30 +86,24 @@ const AlarmClock = (props) => {//extends React.Component {
   		return clearInterval(interval);
   	},[])
 
-  	// componentDidMount() {
-       	
-  	// }
   
-  	// componentWillUnmount() {
-    	
-  	// }	
 
 
-	//render(){
-		return(
-  			<div className={classes.main}>
-  				<p>Budzik</p>
-    			<Clock/>
-    			<Display gardener="23" identy="hour"/>
-    			<Display gardener="59" identy="min"/>
-    			<Display gardener="59" identy="sec"/>
-    			<Button children="Ustaw" classe="buttSet" func={setter}/>
-    			<Times elements={times} deleting={deleting}/>
-    			<audio id="audio" src= "http://greenmp3.pl/dzwonki/3541.mp3"></audio>
-  			</div>
 	
-		);
-	//}
+	return(
+		<div className={classes.main}>
+			<p>Budzik</p>
+			<Clock/>
+			<Display gardener="23" identy="hour"/>
+			<Display gardener="59" identy="min"/>
+			<Display gardener="59" identy="sec"/>
+			<Button children="Ustaw" classe="buttSet" func={setter}/>
+			<Times elements={times} deleting={deleting}/>
+			<audio id="audio" src= "http://greenmp3.pl/dzwonki/3541.mp3"></audio>
+		</div>
+
+	);
+	
 }
 
 
