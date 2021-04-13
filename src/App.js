f8026ef72cf06e9bd8fe4856bb168cd8e843891b
serafinpl <ser.burberibar@gmail.com>
2021-04-13 20:00:46 +0200

    change app component to function

--- src/App.js
+++ src/App.js
@@ -7,27 +7,27 @@ import AlarmClock from "./AlarmClock/AlarmClock.js";
 import CountdownTimer from "./CountdownTimer/CountdownTimer.js";
 import classes from './App.module.css';
 
-class App extends Component {
+const App = () => {
   
  
 
-render() {
+
 
 	return(
-	<React.Fragment>
-		<nav className={classes.navi}>
-			<NavLink to='/alarmclock' activeClassName={classes.active}>Budzik</NavLink>
-			<NavLink to='/countdowntimer' activeClassName={classes.active}>Minutnik</NavLink>
-			
-			<Button children="Stoper" classe="buttSet" />
-			<Button children="Odliczanie" classe="buttSet" />
-		</nav>
-		<Route path='/alarmclock' component={AlarmClock}/>
-		<Route path='/countdowntimer' component={CountdownTimer}/>
-	</React.Fragment>
-		)
+			<React.Fragment>
+				<nav className={classes.navi}>
+					<NavLink to='/alarmclock' activeClassName={classes.active}>Budzik</NavLink>
+					<NavLink to='/countdowntimer' activeClassName={classes.active}>Minutnik</NavLink>
+					
+					<Button children="Stoper" classe="buttSet" />
+					<Button children="Odliczanie" classe="buttSet" />
+				</nav>
+				<Route path='/alarmclock' component={AlarmClock}/>
+				<Route path='/countdowntimer' component={CountdownTimer}/>
+			</React.Fragment>
+		);
 }
-}
+
 
 
 
