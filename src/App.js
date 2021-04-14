f438915a3c2451cdd082627603bc98940760f251
serafinpl <ser.burberibar@gmail.com>
2021-04-14 19:23:28 +0200

    all on function components is done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {NavLink, Route} from 'react-router-dom';
 import Button from "./Button/Button.js";
 import AlarmClock from "./AlarmClock/AlarmClock.js";
 import CountdownTimer from "./CountdownTimer/CountdownTimer.js";
+import CountdownTimerFunc from "./CountdownTimer/CountdownTimerFunc.js";
 import classes from './App.module.css';
 
 const App = () => {
@@ -18,12 +19,12 @@ const App = () => {
 				<nav className={classes.navi}>
 					<NavLink to='/alarmclock' activeClassName={classes.active}>Budzik</NavLink>
 					<NavLink to='/countdowntimer' activeClassName={classes.active}>Minutnik</NavLink>
-					
 					<Button children="Stoper" classe="buttSet" />
 					<Button children="Odliczanie" classe="buttSet" />
 				</nav>
 				<Route path='/alarmclock' component={AlarmClock}/>
-				<Route path='/countdowntimer' component={CountdownTimer}/>
+				<Route path='/countdowntimer' component={CountdownTimerFunc}/>
+				
 			</React.Fragment>
 		);
 }
