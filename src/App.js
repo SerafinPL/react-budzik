719816e8e85eb77b9687d338d325b50d20a4c137
serafinpl <ser.burberibar@gmail.com>
2021-04-15 18:49:11 +0200

    change styling a little

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {NavLink, Route} from 'react-router-dom';
 
 import Button from "./Button/Button.js";
 import AlarmClock from "./AlarmClock/AlarmClock.js";
-
 import CountdownTimerFunc from "./CountdownTimer/CountdownTimerFunc.js";
+import Stopwatch from "./Stopwatch/Stopwatch.js";
 import classes from './App.module.css';
 
 const App = () => {
@@ -19,11 +19,13 @@ const App = () => {
 				<nav className={classes.navi}>
 					<NavLink to='/alarmclock' activeClassName={classes.active}>Budzik</NavLink>
 					<NavLink to='/countdowntimer' activeClassName={classes.active}>Minutnik</NavLink>
-					<Button children="Stoper" classe="buttSet" />
+					<NavLink to='/stopwatch' activeClassName={classes.active}>Stoper</NavLink>
 					<Button children="Odliczanie" classe="buttSet" />
 				</nav>
 				<Route path='/alarmclock' component={AlarmClock}/>
 				<Route path='/countdowntimer' component={CountdownTimerFunc}/>
+				<Route path='/stopwatch' component={Stopwatch}/>
+
 				
 			</React.Fragment>
 		);
